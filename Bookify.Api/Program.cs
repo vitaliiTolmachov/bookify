19531ef16595ab74fd5ff6b2de4942b747bc4372
Vitalii Tolmachov <v.tolmachov@gmail.com>
2024-03-28 13:21:58 +0200

    Disabled seeding data on startup

--- Bookify.Api/Program.cs
+++ Bookify.Api/Program.cs
@@ -21,7 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
     app.ApplyMigrations();
-    app.SeedData();
+    //app.SeedData();
 }
 
 app.UseHttpsRedirection();
