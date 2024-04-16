55e0412be36fd875985df857dae8188f45808c09
Vitalii Tolmachov <v.tolmachov@gmail.com>
2024-04-16 19:46:55 +0300

    Implemented Complete and Reject bookings use-cases

---
+++ Bookify.Application/Bookings/CompleteBooking/CompleteBookingCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Shared;
+
+namespace Bookify.Application.Bookings.CompleteBooking;
+
+internal sealed class CompleteBookingCommandHandler : ICommandHandler<CompleteBookingCommand>
+{
+    private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly IBookingRepository _bookingRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CompleteBookingCommandHandler(
+        IDateTimeProvider dateTimeProvider,
+        IBookingRepository bookingRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _dateTimeProvider = dateTimeProvider;
+        _bookingRepository = bookingRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result> Handle(CompleteBookingCommand request, CancellationToken cancellationToken)
+    {
+        var booking = await _bookingRepository.FindAsync(request.BookingId, cancellationToken);
+
+        if (booking is null)
+        {
+            return Result.Failure(BookingErrors.NotFound(request.BookingId));
+        }
+
+        var result = booking.Complete(_dateTimeProvider.CurrentTimeUtc);
+
+        if (result.IsFailure)
+        {
+            return result;
+        }
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
---
+++ Bookify.Application/Bookings/RejectBooking/RejectBookingCommandCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Shared;
+
+namespace Bookify.Application.Bookings.RejectBooking;
+
+internal sealed class RejectBookingCommandCommandHandler : ICommandHandler<RejectBookingCommand>
+{
+    private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly IBookingRepository _bookingRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RejectBookingCommandCommandHandler(
+        IDateTimeProvider dateTimeProvider,
+        IBookingRepository bookingRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _bookingRepository = bookingRepository;
+        _unitOfWork = unitOfWork;
+        _dateTimeProvider = dateTimeProvider;
+    }
+
+    public async Task<Result> Handle(
+        RejectBookingCommand request,
+        CancellationToken cancellationToken)
+    {
+        var booking = await _bookingRepository.FindAsync(request.BookingId, cancellationToken);
+
+        if (booking is null)
+        {
+            return Result.Failure(BookingErrors.NotFound(request.BookingId));
+        }
+
+        var result = booking.Reject(_dateTimeProvider.CurrentTimeUtc);
+
+        if (result.IsFailure)
+        {
+            return result;
+        }
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
